7dfbea52467f5189b4548c6a96e0fcf82719748e
horacioskrp <hervekudayah@gmail.com>
2024-08-09 01:14:15 +0000

    🌟Add model and Jpa repository

--- src/main/java/com/skrp/batchio/entity/Industry.java
+++ src/main/java/com/skrp/batchio/entity/Industry.java
@@ -1,4 +1,23 @@
 package com.skrp.batchio.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Industry")
 public class Industry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private int year;
+    private String industryAggregationNZSIOC;
+    private String industryCodeNZSIOC;
+    private String industryNameNZSIOC;
+    private String units;
+    private String variableCode;
+    private String variableName;
+    private String variableCategory;
+    private long value;
+    private String industryCodeANZSIC06;
+
 }
---
+++ src/main/java/com/skrp/batchio/repository/IndustryRepository.java
@@ -0,0 +1,7 @@
+package com.skrp.batchio.repository;
+
+import com.skrp.batchio.entity.Industry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndustryRepository extends JpaRepository<Industry, Long> {
+}
